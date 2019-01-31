8fb1968f261c33c2370265265c71226d78396ae3
yaochao <yaochaoutokyo@gmail.com>
2019-01-31 19:47:20 +0900

    fragment

--- RecycleViewDemo/app/src/main/java/com/example/recycleviewdemo/MainActivity.java
+++ RecycleViewDemo/app/src/main/java/com/example/recycleviewdemo/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.recycleviewdemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // specify an adapter
+        mAdapter = new MyAdapter(mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    protected void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private List<String> mDataset;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public MyViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+        public MyAdapter(List<String> myDataset) {
+            mDataset = myDataset;
+        }
+        @NonNull
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.activity_main  , parent, false);
+            MyViewHolder myViewHolder = new MyViewHolder(textView);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder myViewHolder, int position) {
+            myViewHolder.mTextView.setText(mDataset.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
     }
 }
